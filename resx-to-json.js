6139d97166d2572b2333d9a1a52304552a154f4b
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2016-02-14 11:05:46 -0800

    refactor so module return is up top

--- resx-to-json.js
+++ resx-to-json.js
@@ -5,17 +5,9 @@ var path = require('path');
 var _ = require('lodash');
 var fs = require('fs');
 
-function nggettextLocaleFormatter(locale) {
-  if (locale.length > 2) {
-    var split = locale.split('-');
-    locale = split[0].toLowerCase() + '_' + split[1].toUpperCase();
-  }
-  return locale;
-}
-
-function defaultLocaleFormatter(locale) {
-  return locale;
-}
+module.exports = {
+  Converter: ResxToJsonConverter,
+};
 
 function ResxToJsonConverter(options) {
   this.options = _.defaults(options, {
@@ -35,48 +27,57 @@ function ResxToJsonConverter(options) {
   }
 }
 
-ResxToJsonConverter.prototype.fromXmlString =
-  function(xmlString, locale, callback) {
-    var parser = new xml2js.Parser();
-    parser.parseString(xmlString, function(err, parsed) {
-      if (err) {
-        return callback(err, null);
-      }
+ResxToJsonConverter.prototype.fromXmlString = ConverterFromXmlString;
+ResxToJsonConverter.prototype.determineLocale = ConverterDetermineLocale;
+ResxToJsonConverter.prototype.fromResxFile = ConverterFromResxFile;
 
-      var dictionary = {};
-      parsed.root.data.forEach(function(nameValuePair) {
-        dictionary[nameValuePair.$.name] = nameValuePair.value[0];
-      });
-
-      var result = {};
-      result[locale] = dictionary;
-      return callback(null, result);
-    });
-  };
-
-ResxToJsonConverter.prototype.determineLocale =
-  function(resxFilePath) {
-    var locale = path.extname(path.parse(resxFilePath).name);
-    if (!locale) {
-      locale = this.options.defaultLocale;
-    } else {
-      locale = locale.substring(1);
+function ConverterFromXmlString(xmlString, locale, callback) {
+  var parser = new xml2js.Parser();
+  parser.parseString(xmlString, function(err, parsed) {
+    if (err) {
+      return callback(err, null);
     }
-    return this.localeFormatter(locale);
-  };
 
-ResxToJsonConverter.prototype.fromResxFile =
-  function(resxFilePath, callback) {
-    var locale = this.determineLocale(resxFilePath);
-    var self = this;
-    fs.readFile(resxFilePath, function(err, fileContents) {
-      if (err) {
-        return callback(err, null);
-      }
-      return self.fromXmlString(fileContents, locale, callback);
+    var dictionary = {};
+    parsed.root.data.forEach(function(nameValuePair) {
+      dictionary[nameValuePair.$.name] = nameValuePair.value[0];
     });
-  };
 
-module.exports = {
-  Converter: ResxToJsonConverter,
-};
+    var result = {};
+    result[locale] = dictionary;
+    return callback(null, result);
+  });
+}
+
+function ConverterDetermineLocale(resxFilePath) {
+  var locale = path.extname(path.parse(resxFilePath).name);
+  if (!locale) {
+    locale = this.options.defaultLocale;
+  } else {
+    locale = locale.substring(1);
+  }
+  return this.localeFormatter(locale);
+}
+
+function ConverterFromResxFile(resxFilePath, callback) {
+  var locale = this.determineLocale(resxFilePath);
+  var self = this;
+  fs.readFile(resxFilePath, function(err, fileContents) {
+    if (err) {
+      return callback(err, null);
+    }
+    return self.fromXmlString(fileContents, locale, callback);
+  });
+}
+
+function nggettextLocaleFormatter(locale) {
+  if (locale.length > 2) {
+    var split = locale.split('-');
+    locale = split[0].toLowerCase() + '_' + split[1].toUpperCase();
+  }
+  return locale;
+}
+
+function defaultLocaleFormatter(locale) {
+  return locale;
+}
